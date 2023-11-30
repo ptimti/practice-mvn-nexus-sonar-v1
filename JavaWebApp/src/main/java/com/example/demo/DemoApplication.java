b187487ad4993ab040eabf24e3834668fe048847
ptimti <placidtimti54@gmail.com>
2023-11-30 22:12:53 +0000

    fix for syntax issue

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,12 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name ="placid";
+		String name ="placid";
 
-		system.out.println(name);
-
-		Integer age = "20";
-		System.out.println(age + 5);
+		System.out.println(name);
 	}
 
 }
